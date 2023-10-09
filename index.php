c4a45129e6a2c22b8187a35d93b3f74cc9918343
--replace-all <--replace-all>
2023-10-09 13:43:55 +0200

    Update #1

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$title = "RATP | DasniloYT";
-require "templates/start.php";
-require "functions.php";
-
-welcome();
-
-require "templates/end.php";
-?>
--- templates/end.php
+++ templates/end.php
@@ -1,3 +1,4 @@
 <script src="script.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 </body>
 </html>
