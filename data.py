9b019d41cce32e06cd05ee2fc75c604b53b9e64d
--replace-all <--replace-all>
2023-10-12 15:27:46 +0200

    Update #?? - Add datas (rail/metro/tram)

--- data.py
+++ data.py
@@ -2,16 +2,15 @@ import json
 
 datatr = json.load(open("data/data-tr.json", "r", encoding="utf-8"))
 lines = json.load(open("data/lines.json", "r", encoding="utf-8"))
-blines = {}
-bstops = {}
-
+mlines = {}
+mstops = {}
 
 for line in lines:
-    if line["transportmode"] == "bus":
+    if line["transportmode"] == "rail":
         for arret_tr in datatr:
             x = arret_tr["line"].split(":")[-2]
-            if x == line["id_line"] and line["name_line"] not in blines.keys():
-                blines[line["name_line"]] = line["id_line"]
+            if x == line["id_line"] and line["name_line"] not in mlines.keys():
+                mlines[line["name_line"]] = line["id_line"]
 
 
 for arret_tr in datatr:
@@ -19,14 +18,14 @@ for arret_tr in datatr:
     stopId = arret_tr["stopPointRef"].split(":")[-2]
     stopName = arret_tr["stopName"]
     try:
-        if lineId in blines.values() and lineId not in bstops:
-            bstops[lineId] = {}
-        if stopName not in bstops[lineId] and lineId in blines.values():
-            bstops[lineId][stopName] = [stopId]
+        if lineId in mlines.values() and lineId not in mstops:
+            mstops[lineId] = {}
+        if stopName not in mstops[lineId] and lineId in mlines.values():
+            mstops[lineId][stopName] = [stopId]
         else:
-            bstops[lineId][stopName].append(stopId)
+            mstops[lineId][stopName].append(stopId)
     except:
         continue
     
-json.dump(blines, open("data/blines2.json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
-json.dump(bstops, open("data/bstops2.json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
+json.dump(mlines, open("data/rlines.json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
+json.dump(mstops, open("data/rstops.json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
--- templates/end.php
+++
@@ -1,4 +0,0 @@
-<script src="script.js"></script>
-<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-</body>
-</html>
