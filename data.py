562ae7c6ca5fedb5b1e6e716cb4dc4af88099516
--replace-all <--replace-all>
2023-10-12 15:56:58 +0200

    #Update ??+1 - Full lines & stops data

--- data.py
+++ data.py
@@ -1,31 +1,47 @@
 import json
 
 datatr = json.load(open("data/data-tr.json", "r", encoding="utf-8"))
-lines = json.load(open("data/lines.json", "r", encoding="utf-8"))
-mlines = {}
-mstops = {}
+lines = json.load(open("data/data-lines.json", "r", encoding="utf-8"))
+full_lines = {"bus": {}, "metro": {}, "rail": {}, "tram": {}} # séparés par les différents catégories
+full_stops = {} # tous mélangés (vu qu'on se base sur les clés (lineId))
+
+# Oublie pas de supprimer les différents xlines et xstops
 
 for line in lines:
-    if line["transportmode"] == "rail":
+    if line["transportmode"] == "bus":
         for arret_tr in datatr:
             x = arret_tr["line"].split(":")[-2]
-            if x == line["id_line"] and line["name_line"] not in mlines.keys():
-                mlines[line["name_line"]] = line["id_line"]
-
+            if x == line["id_line"] and line["name_line"] not in full_lines.keys():
+                full_lines["bus"][line["name_line"]] = line["id_line"]
+    elif line["transportmode"] == "metro":
+        for arret_tr in datatr:
+            x = arret_tr["line"].split(":")[-2]
+            if x == line["id_line"] and line["name_line"] not in full_lines.keys():
+                full_lines["metro"][line["name_line"]] = line["id_line"]
+    elif line["transportmode"] == "rail":
+        for arret_tr in datatr:
+            x = arret_tr["line"].split(":")[-2]
+            if x == line["id_line"] and line["name_line"] not in full_lines.keys():
+                full_lines["rail"][line["name_line"]] = line["id_line"]
+    elif line["transportmode"] == "tram":
+        for arret_tr in datatr:
+            x = arret_tr["line"].split(":")[-2]
+            if x == line["id_line"] and line["name_line"] not in full_lines.keys():
+                full_lines["tram"][line["name_line"]] = line["id_line"]
 
 for arret_tr in datatr:
     lineId = arret_tr["line"].split(":")[-2]
     stopId = arret_tr["stopPointRef"].split(":")[-2]
     stopName = arret_tr["stopName"]
     try:
-        if lineId in mlines.values() and lineId not in mstops:
-            mstops[lineId] = {}
-        if stopName not in mstops[lineId] and lineId in mlines.values():
-            mstops[lineId][stopName] = [stopId]
+        if lineId in full_lines.values() and lineId not in full_stops:
+            full_stops[lineId] = {}
+        if stopName not in full_stops[lineId] and lineId in full_lines.values():
+            full_stops[lineId][stopName] = [stopId]
         else:
-            mstops[lineId][stopName].append(stopId)
+            full_stops[lineId][stopName].append(stopId)
     except:
         continue
     
-json.dump(mlines, open("data/rlines.json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
-json.dump(mstops, open("data/rstops.json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
+json.dump(full_lines, open("data/lines.json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
+json.dump(full_stops, open("data/stops.json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
